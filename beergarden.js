a8d3b26073dcfc71e8b20023ff7c90f06e5e8d0a
enricogigantiello <enrico.gigantiello@gmail.com>
2015-08-21 18:23:25 +0200

    added orders

--- beergarden.js
+++ beergarden.js
@@ -1,10 +1,18 @@
 Products = new Mongo.Collection("products");
-Router.configure({
-    layoutTemplate: 'main'
+Orders = new Meteor.Collection('orders');
+
+Router.configure({ layoutTemplate: 'main'});
+
+Router.route('/order/:_id', {
+  template: 'listOrder',
+    data: function(){
+      var currentOrder = this.params._id;
+      return Orders.findOne({ _id: currentOrder });
+       
+    }
 });
-
-
-Router.route('/register');
+Router.route('/manageProducts');
+Router.route('/orders');
 
 
 Router.route('/', {
@@ -15,26 +23,27 @@ Router.route('/', {
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Template.body.helpers({
+  Template.manageProducts.helpers({
     products: function () {
-      return Products.find();
+       //return Products.find();
+      return Products.find({}, {sort: {type: 1, name: 1, price: 1}});
     }
   });
 
-  Template.body.events({
+  Template.manageProducts.events({
     "submit .new-prod": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
  
       // Get value from form element
       var name = event.target.name.value;
-      var price = parseInt(event.target.price.value);
+      var price = parseFloat(event.target.price.value);
       var image = event.target.image.value;
-      var volume = parseFloat(event.target.volume.value);
+      var volume = parseFloat(event.target.volume.tvalue);
       var type = event.target.type.value;
       var description = event.target.description.value;
+      var sellable = event.target.sellable.checked;
     
-     
       // Insert a task into the collection
       Products.insert({
         name: name,
@@ -43,7 +52,8 @@ if (Meteor.isClient) {
         volume: volume,
         type: type,
         description: description,
-        finished: false
+        finished: false,
+        sellable: sellable
         
         //createdAt: new Date() // current time
       });
@@ -56,29 +66,44 @@ if (Meteor.isClient) {
       event.target.volume.value= "";
       event.target.type.value= "";
       event.target.description.value= "";
+      event.target.sellable.checked= false;
     }
   });
 
   Template.product.helpers({
-    'checked': function(){
+    'isfinished': function(){
         var isFinished = this.finished;
         if(isFinished){
             return "checked";
         } else {
             return "";
         }
+    },
+    'issellable': function(){
+        var issellable = this.sellable;
+        if(issellable){
+            return "checked";
+        } else {
+            return "";
+        }
     }
-});
+  });
 
   Template.product.events({
-    "click .toggle-checked": function () {
+    "click .toggle-finished": function () {
       // Set the checked property to the opposite of its current value
       Products.update(this._id, {
         $set: {finished: ! this.finished}
       });
     },
+    "click .toggle-sellable": function () {
+      // Set the checked property to the opposite of its current value
+      Products.update(this._id, {
+        $set: {sellable: ! this.sellable}
+      });
+    },
     "click .delete": function () {
-      var confirm = window.confirm("Delete this task?");
+      var confirm = window.confirm("Delete this product?");
       if(confirm){
       Products.remove(this._id);
       }
@@ -100,10 +125,115 @@ if (Meteor.isClient) {
     var prod_elem = $(event.target).val();
     Products.update({ _id: documentId }, {$set: { volume: prod_elem }});
     //console.log("Task changed to: " + todoItem);
+    },
+    'keyup [name=image]': function(event){
+    var documentId = this._id;
+    var prod_elem = $(event.target).val();
+    Products.update({ _id: documentId }, {$set: { image: prod_elem }});
+    //console.log("Task changed to: " + todoItem);
+    },
+    'keyup [name=type]': function(event){
+    var documentId = this._id;
+    var prod_elem = $(event.target).val();
+    Products.update({ _id: documentId }, {$set: { type: prod_elem }});
+    //console.log("Task changed to: " + todoItem);
+    },
+    'keyup [name=description]': function(event){
+    var documentId = this._id;
+    var prod_elem = $(event.target).val();
+    Products.update({ _id: documentId }, {$set: { description: prod_elem }});
+    //console.log("Task changed to: " + todoItem);
     }
   });
+
+/**
+*Orders
+*
+**/
+  Template.addOrder.events({
+      'submit form': function(event){
+        event.preventDefault();
+        var guestName = $('[name=guestName]').val();
+        var tableNum = $('[name=tableNum]').val();
+        
+        Orders.insert({
+            guestName: guestName,
+            tableNum: tableNum,
+            createdAt: new Date(),
+            bill: 0.0,
+            payed: false,
+            items: [] 
+        });
+        $('[name=guestName]').val('');
+        $('[name=tableNum]').val('');
+      }
+  });
+
+  Template.orders.helpers({
+      'orders': function(){
+          return Orders.find({}, {sort: {guestName: 1}});
+      }
+
+  });
+
+  Template.orders.events({
+    "click .selectOrder": function () {
+
+      Session.set('selectedOrder', this._id);
+      
+    }
+
+  });
+
+  Template.menu.helpers({
+     
+      'menu_products': function(){
+          return Products.find({ finished: false, sellable: true}, {sort: {type: 1}});
+      }
+
+  });
+
+  Template.menu.events({
+    "click .add": function () {
+      var selectedOrder = Session.get('selectedOrder');
+      
+      if(selectedOrder != null){
+        var item = {
+        product_id:  this._id,
+        name: this.name,
+        volume: this.volume,
+        price:  this.price,
+        image:  this.image, 
+        quantity: 1
+      
+      };
+        
+        console.log(item);
+        Orders.update(selectedOrder, {
+          $push: { items: item },
+          $inc: { bill: parseFloat(item.price) }
+        
+        }
+        );
+      }
+      else alert("didn't choose an order");
+      
+    }
+
+  });
+
 }
 
 if(Meteor.isServer){
     // server code goes here
+    // Global API configuration
+  var Api = new Restivus({
+    useDefaultAuth: true,
+    prettyJson: true
+  });
+
+  // Generates: GET, POST on /api/coll and GET, PUT, DELETE on
+  // /api/coll/:id for Items collection
+  Api.addCollection(Products);
+  Api.addCollection(Orders);
 }
